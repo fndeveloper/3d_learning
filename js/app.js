e1c439a91bd95eb4ec9911255cd91080a28f98e1
fndeveloper <fasihnasir60@gmail.com>
2025-08-01 21:24:41 +0500

    c

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+
 // ===================== HEADER CODE START ======================
 document.addEventListener("DOMContentLoaded", () => {
 
@@ -11,24 +12,53 @@ if (header) {
     .then((e) => e.text())
     .then((data) => {
       header.innerHTML = data;
-
-      // ============= CODE FOR CHECK COLLPASE OF SHEDULE APPOINTMENT START ===============
-setTimeout(() => {
-const el = document.querySelector(".shedule_collapse");
-if (el && el.offsetHeight > 0) {
-  console.log("Collapse is OPEN (visible)");
-} else {
-  console.log("Collapse is CLOSED (hidden)");
-}
-
-}, 500);
-// ============= CODE FOR CHECK COLLPASE OF SHEDULE APPOINTMENT END ===============
+// =============== TO CHECK A CURRENT USER LOGIN COCDE START ======================
+var current_use=JSON.parse(localStorage.getItem("current_user_data_docs"))
+var profile_div=document.getElementById("profile_div");
+var porfile_modal_body=document.getElementById("porfile_modal_body");
 
 
+if(current_use){
+  console.log(current_use);
+  
+profile_div.innerHTML=`
+<button type="button"  class="  border-0 rounded-3 px-3 d-flex flex-row align-items-center"
+    data-bs-toggle="modal" data-bs-target="#exampleModal">
+<img src="${current_use.profiel_pic ? current_use.profiel_pic: "https://cdn.pixabay.com/photo/2023/02/18/11/00/icon-7797704_1280.png"}" class="img-fluid col-10 rounded-circle" alt="">
 
-      initNavbarTabHover?.(); 
+    <i class="fa-solid fa-caret-down"></i>
+</button>
+
+`
+
+porfile_modal_body.innerHTML=`
+   <div class="d-flex flex-column align-items-center mb-2">
+        <img src="${current_use.profiel_pic ? current_use.profiel_pic: "https://cdn.pixabay.com/photo/2023/02/18/11/00/icon-7797704_1280.png"}" class="img-fluid col-3 rounded-circle" alt=""> 
+        <h6 class="py-2">${current_use.name}</h6>
+      </div>
+
+      <div>
+ 
+        <p class"fw-lighter"> Email : ${current_use.email_user}</p>
+        <p class"fw-lighter">Qualification : ${current_use.qualification}</p>
+     <p class"fw-lighter">Level: ${current_use.Account_Type}</p>
+     
+     <p class"fw-lighter">Feild : ${current_use.fieldOfStudy}</p>
+      </div>
+      </div>
+      <div class="modal-footer d-flex flex-row justify-content-center align-items-center">
+        <button type="button" class="btn col-lg-6 col-10 bg_light text-dark "  id="logout_user">
+        Logout
+        </button>
+      </div>
+`
 
 
+} 
+
+// =============== TO CHECK A CURRENT USER LOGIN COCDE END ======================
+
+   
       document.querySelectorAll('a[href]').forEach(link => {
         const href = link.getAttribute('href');
 
@@ -60,4 +90,6 @@ if(footer){
   footer.innerHTML=data})
 }
 // ===================== HEADER CODE END ======================
+
+
 
--- js/auth.js
+++ js/auth.js
@@ -1,9 +1,7 @@
 
-
-  // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
-  import { getAuth ,createUserWithEmailAndPassword,signInWithEmailAndPassword ,sendEmailVerification } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
-
+  import { signOut ,onAuthStateChanged,getAuth,setPersistence,browserSessionPersistence,browserLocalPersistence ,createUserWithEmailAndPassword,signInWithEmailAndPassword ,sendEmailVerification } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+  import { collection, addDoc ,getFirestore, doc, getDoc , getDocs } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js"; 
 
   const firebaseConfig = {
     apiKey: "AIzaSyATKP7NSf-dBNYu9cwpGKvUvh3X09GlnZw",
@@ -17,8 +15,26 @@
 
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
   const Auth = getAuth(app);
+// ============== FIND A CURRENT USER CODE START =========================
 
+onAuthStateChanged(Auth, (user) => {
+  if (user) {
+    console.log(user);
+    localStorage.setItem("current_user",JSON.stringify(user))
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/auth.user
+    const uid = user.uid;
+    // ...
+  } else {
+    // User is signed out
+    // ...
+  }
+});
+// ============== FIND A CURRENT USER CODE END =========================
+
+console.log();
 // =================== LOGIN CODE IS START HERE ============================
 var submit_signup=document.getElementById("submit_signup");
 submit_signup.addEventListener("click",(e)=>{
@@ -28,20 +44,47 @@ var signup_form=document.getElementById("signup_form");
 
 
 
-createUserWithEmailAndPassword(Auth, signup_form.querySelector("#email").value, signup_form.querySelector("#password").value)
+ createUserWithEmailAndPassword(Auth, signup_form.querySelector("#email").value, signup_form.querySelector("#password").value)
   .then((userCredential) => {
     // Signed up 
     const user = userCredential.user;
+
+    // =========== SEND A DATA OF ACCOUTN CREATION START ================
+    const docRef = addDoc(collection(db, "users"), {
+name:signup_form.querySelector("#email").value,
+      email_user:user.email,
+id_user:user.uid, 
+Account_Type:signup_form.querySelector("#accountType").value,
+fieldOfStudy:signup_form.querySelector("#fieldOfStudy").value,
+qualification:signup_form.querySelector("#qualification").value,
+profiel_pic:signup_form.querySelector("#pic_url").value,
+date:Date().split(" ").slice(0,5).join(" "),
+
+});
+
+    // =========== SEND A DATA OF ACCOUUNT CREATION END ================-=-
      sendEmailVerification(user)
       .then(() => {
-        alert("Verification email sent. Please check your inbox.");
-         Auth.signOut();
+        Swal.fire({
+  icon: 'success',
+  title: 'Account is Create successfully!',
+  text:'For Sign-In Verification email sent. Please check your inbox',
+  showConfirmButton: true,
+
+});
+signup_form.reset();
+
       });
-    console.log(user);
     
     // ...
   })
   .catch((error) => {
+    Swal.fire({
+  icon: "error",
+  title: "Oops...",
+  text: `Something went wrong! ${error}`,
+
+});
     const errorCode = error.code;
     const errorMessage = error.message;
     // ..
@@ -54,31 +97,118 @@ var submit_login=document.getElementById("submit_login");
 submit_login.addEventListener("click",(e)=>{
   e.preventDefault()
 var login_form=document.getElementById("login_form");
+  const rememberMe = document.getElementById("rememberMe").checked;
+ const persistenceType = rememberMe ? browserLocalPersistence : browserSessionPersistence;
+setPersistence(Auth,  persistenceType)
+  .then(() => {
+   
+    return signInWithEmailAndPassword(Auth, login_form.querySelector("#email").value, login_form.querySelector("#password").value)
+  })
+ 
 
-
-
-
-  signInWithEmailAndPassword(Auth, login_form.querySelector("#email").value, login_form.querySelector("#password").value)
   .then((userCredential) => {
     // Signed in 
     const user = userCredential.user;
     console.log(user);
+    
     if(!user.emailVerified){
-      alert("first Vereifed your email")
+
+        Swal.fire({
+  icon: "error",
+  title: "Oops...",
+  text: `First Vereifed Your Email `,
+
+});
     }
     else{
-      alert("success");
-      window.location.href="index.html"
-      localStorage.setItem("user",JSON.parse(user))
+   
+      Swal.fire({
+  title: "Login Successfull",
+  icon: "success",
+  draggable: true
+});
+setTimeout(() => {
+        window.location.href="index.html"
+}, 2500);
+  
     }
     // ...
   })
   .catch((error) => {
-    console.log(error.message);
-    
+
+    Swal.fire({
+  icon: "error",
+  title: "Oops...",
+  text: `Something went wrong! ${error.message}`,
+
+});
 
   });
 
 })
 
 // =================== LOGIN CODE IS END HERE =================================
+document.getElementById("goToSignup").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  
+  document.querySelectorAll('.tab-pane').forEach(pane => {
+    pane.classList.remove('show', 'active');
+  });
+
+  const signupTab = document.getElementById("profile");
+  signupTab.classList.add("show", "active");
+
+  document.querySelectorAll('.nav-link').forEach(tab => {
+    tab.classList.remove('active');
+  });
+  document.querySelector('[data-bs-target="#profile"]').classList.add('active');
+});
+
+
+// ================================ GO TO SIGN IN =======================
+document.getElementById("goToSigin").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  
+  document.querySelectorAll('.tab-pane').forEach(pane => {
+    pane.classList.add('show', 'active');
+  });
+
+  const signupTab = document.getElementById("profile");
+  signupTab.classList.remove("show", "active");
+
+  document.querySelectorAll('.nav-link').forEach(tab => {
+    tab.classList.add('active');
+  });
+  document.querySelector('[data-bs-target="#profile"]').classList.remove('active');
+});
+
+
+
+// ================================= GET A DATA FROM DATABSE OF USERS START =============================================
+const querySnapshot = await getDocs(collection(db, "users"));
+querySnapshot.forEach((doc) => {
+  // doc.data() is never undefined for query doc snapshots
+  // console.log(JSON.parse(localStorage.getItem("current_user")).uid);
+  if(doc.data().id_user === JSON.parse(localStorage.getItem("current_user")).uid ){
+    console.log(doc.data())
+    localStorage.setItem("current_user_data_docs",JSON.stringify(doc.data()))
+  }
+});
+
+
+
+// ================================= GET A DATA FROM DATABSE OF USERS END =============================================
+
+
+var logout_user=document.getElementById("logout_user");
+logout_user.addEventListener("click",()=>{
+  console.log("ss");
+  
+  signOut(Auth).then(() => {
+alert("signout")
+}).catch((error) => {
+  // An error happened.
+});
+})
